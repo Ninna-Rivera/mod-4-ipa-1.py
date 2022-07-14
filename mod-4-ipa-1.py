4a8034443fa4128b4b84639386e478594e035f58
Ninna-Rivera <ninna.rivera@obf.ateneo.edu>
2022-07-14 20:38:00 +0800

    second commit

--- mod-4-ipa-1.py
+++ mod-4-ipa-1.py
@@ -33,8 +33,16 @@ def relationship_status(from_member, to_member, social_graph):
     '''
     # Replace `pass` with your code. 
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
-
+    from_member_follows = social_graph[from_member]["following"]
+    to_member_follows = social_graph[to_member]["following"]
+    if to_member in from_member_follows and from_member in to_member_follows:
+        return ("friends")
+    elif to_member in from_member_follows:
+        return ("follower")
+    elif from_member in to_member_follows:
+        return ("followed by")
+    else:
+        return ("no relationship")
 
 def tic_tac_toe(board):
     '''Tic Tac Toe. 
@@ -57,7 +65,39 @@ def tic_tac_toe(board):
     '''
     # Replace `pass` with your code. 
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    N = len(board)
+
+    #1 check for winners horizontally
+    for i in range (N):
+        row =[]
+        for j in range (N):
+            row.append(board[i][j])
+        if row.count(row[0]) == len (row):
+                return row[0]
+        
+    #2 check for winners vertically
+    for j in range (N):
+        column =[]
+        for i in range (N):
+            column.append(board[i][j])
+        if column.count(column[0]) == len (column):
+                return column[0]
+        
+    #3 check for winners diagonally
+    diagonal1_UL_LR = []
+    for i in range (N):
+        diagonal1_UL_LR.append(board[i][i])
+    if diagonal1_UL_LR.count(diagonal1_UL_LR[0]) == len (diagonal1_UL_LR):
+            return diagonal1_UL_LR[0]
+
+    diagonal2_LL_UR = []
+    for i in range (N):
+        diagonal2_LL_UR.append(board[2-i][i])
+    if diagonal2_LL_UR.count(diagonal2_LL_UR[0]) == len (diagonal2_LL_UR):
+            return diagonal2_LL_UR[0]
+        
+    #4 If no winners return "NO WINNER"
+    return "NO WINNER"
 
 def eta(first_stop, second_stop, route_map):
     '''ETA. 
@@ -85,5 +125,20 @@ def eta(first_stop, second_stop, route_map):
     '''
     # Replace `pass` with your code. 
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
-Footer
+    route_map_list = list(route_map)
+    N = len(route_map_list)
+    distance = []
+    
+    current_stop = first_stop
+    while current_stop != second_stop:
+        #find the next stop
+            next_stop = ""
+            for i in range (N):
+                if route_map_list[i][0] == current_stop:
+                    next_stop = route_map_list[i][1]
+    #add to the travel time distance between (current stop, next stop)
+                    distance.append(route_map[route_map_list[i]]["travel_time_mins"])
+                    distance
+    #go to the next stop
+            current_stop = next_stop
+    return (sum(distance))
